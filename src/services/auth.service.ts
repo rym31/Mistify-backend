83a6d7bc20f4a6797f2c41d0fd7b8236f4decaf2
rym31 <rym4085@outlook.com>
2026-02-25 15:54:56 -0500

    Petit changement inteceptors

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,16 +3,18 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
 
-    //1.Check if email is in use
+    signin() {
+        //1.Check if email is in use
 
-    //2.hash the password
+        //2.hash the password
 
-    //3.create a new user and save it
+        //3.create a new user and save it
 
-    //4. return the user
-    // return 'This action will sign in a user';
+        //4. return the user
+        return 'This action will sign in a user';
+    }
+
+    signup() {
+        return 'This action will sign up a user';
+    }
 }
-
-    // signup() {
-    //     return 'This action will sign up a user';
-    // }
