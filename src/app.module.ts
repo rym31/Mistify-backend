9366589e26ce8f385fbee278c47d339f9f8640b9
rym31 <rym4085@outlook.com>
2026-04-01 02:06:03 -0400

    Resolve merge conflict in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
-<<<<<<< HEAD
 import { ParfumsModule } from './parfums/parfums.module';
 import { Parfum } from './parfums/parfum.entity';
 import { CommentairesModule } from './commentaires/commentaires.module';
@@ -15,6 +14,7 @@ import { OffresModule } from './offres/offres.module';
 import { Offre } from './offres/offre.entity';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
@@ -34,19 +34,6 @@ import { join } from 'path';
   ],
   controllers: [AppController],
   providers: [AppService],
-=======
-import { AuthService } from './services/auth.service';
- 
-@Module({
-  imports: [TypeOrmModule.forRoot({
-    type:'sqlite',
-    database : 'db.sqlite',
-    autoLoadEntities : true,
-    synchronize : true
-  }),UsersModule],
-  controllers: [AppController, ],
-  providers: [AppService, AuthService],
->>>>>>> 83a6d7bc20f4a6797f2c41d0fd7b8236f4decaf2
 })
 export class AppModule {}
  
