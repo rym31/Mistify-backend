e9a1ebb0d3687dc5161d648f10cf03f421710905
Ellyn Saint-Firmin <6309487@cegepmv.ca>
2026-05-04 21:17:00 -0400

    ajout statut demande

--- src/ajoutParfum/ajoutParfum.entity.ts
+++ src/ajoutParfum/ajoutParfum.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { User } from 'src/users/user.entity';
+import {Statut} from "./Statut"
 
 @Entity()
 export class AjoutParfum {
@@ -23,22 +24,13 @@ export class AjoutParfum {
   price?: number;
 
   @Column({ nullable: true })
-  country?: string;
-
-  @Column({ nullable: true })
   gender?: string;
 
   @Column({ nullable: true, type: 'int' })
   year?: number;
 
-  @Column({ nullable: true })
-  topNotes?: string;
-
-  @Column({ nullable: true })
-  middleNotes?: string;
-
-  @Column({ nullable: true })
-  baseNotes?: string;
+  @Column() 
+  statut.EN_ANTTENTE: Enumerator
   
   @ManyToOne(() => User, { onDelete: 'CASCADE' })
   user: User;
