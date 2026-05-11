e0f4d3cd79fb2338cc20d1fa793e6e0057bbf326
yanis26x <yanis.djenadi@hotmail.com>
2026-05-11 08:54:19 -0400

    yea

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { PanierModule } from './panier/panier.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'public'),
+      rootPath: join(__dirname, '..', 'public'),
     }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
