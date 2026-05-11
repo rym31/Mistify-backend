52ead5646e9f7257fd376e3a7985cfa9fee29e84
yanis26x <yanis.djenadi@hotmail.com>
2026-05-11 15:26:44 -0400

    finit

--- src/panier/panier.entity.ts
+++ src/panier/panier.entity.ts
@@ -10,9 +10,9 @@ export class Panier {
   @Column({ default: 1 })
   quantite: number;
 
-  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @ManyToOne(() => User, { onDelete: 'CASCADE' }) //CASCADE = si le parfum parent est supprimé, toutes les entités liées à ce parfum seront automatiquement supprimées par la base de données.
   user: User;
 
-  @ManyToOne(() => Parfum, { onDelete: 'CASCADE' })
+  @ManyToOne(() => Parfum, { onDelete: 'CASCADE' }) //CASCADE = si le parfum parent est supprimé, toutes les entités liées à ce parfum seront automatiquement supprimées par la base de données.
   parfum: Parfum;
 }
